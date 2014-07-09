f95ce2ccf691ee95c43ce33cb2f4fe77703c422b
irisviel <sollrei@163.com>
2014-07-09 17:28:01 +0800

    =V=

--- models/post.js
+++ models/post.js
@@ -18,6 +18,8 @@ Post.prototype.save = function (callback) {
     var time = {
         date: date,
         year: date.getFullYear(),
+        sMonth: date.getMonth() + 1,
+        sDay: date.getDate(),
         month: date.getFullYear() + '-' + (date.getMonth() + 1),
         day: date.getFullYear() + '-' + (date.getMonth() + 1) + '-' + date.getDate(),
         minute: date.getFullYear() + '-' + (date.getMonth() + 1) + '-' + date.getDate() + " " +  date.getHours() + ':' + (date.getMinutes() < 10 ? '0' + date.getMinutes() : date.getMinutes())
